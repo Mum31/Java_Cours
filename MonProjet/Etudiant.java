aee0dda6738fa4e171410d21423934a42aa82d87
Mum31 <168490361+Mum31@users.noreply.github.com>
2025-02-18 13:03:50 +0100

    mon-app

--- MonProjet/Etudiant.java
+++ MonProjet/Etudiant.java
@@ -1,21 +1,18 @@
 package MonProjet;
-import java.util.Scanner;
 
 public class Etudiant {
-    public String nom;
-    public String prenom;
-    public String classe;
+    private String nom;
+    private String prenom;
+    private String classe;
 
     public Etudiant(String pnom, String pprenom, String pclasse) {
         this.nom = pnom;
         this.prenom = pprenom;
         this.classe = pclasse;
-
     }
+
     @Override
     public String toString() {
-        return this.nom + " " + this.prenom + " " + this.classe;
+        return this.nom + " " + this.prenom + " en " + this.classe;
     }
-  
-   
 }
